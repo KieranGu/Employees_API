4fb7fdd38d85ed3289e7077dc019d88996f0745c
KieranGu <kieran.gu@oocl.com>
2025-08-26 21:37:30 +0800

    feat:male employees and get employee by id

--- src/main/java/com/oocl/traning/Controller/CompanyController.java
+++ src/main/java/com/oocl/traning/Controller/CompanyController.java
@@ -6,31 +6,55 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/api/v1")
 public class CompanyController {
-    private final static Map<Integer,Company> companies = new HashMap<>();
+    private final static Map<Integer, Company> companies = new HashMap<>();
     private final static ArrayList<Employee> allEmployees = new ArrayList<>();
+
     @PostMapping("/employees")
-    public void postEmployee(Employee employee){
-        employee.setEmployeeID(allEmployees.size()+1);
-        allEmployees.set(allEmployees.size()+1,employee);
+    @ResponseStatus(HttpStatus.CREATED)
+    public void postEmployee(Employee employee) {
+        employee.setEmployeeID(allEmployees.size() + 1);
+        allEmployees.set(allEmployees.size() + 1, employee);
     }
 
+    @GetMapping("/employees/1")
+    public Employee getEmployeeById(int id) {
+        for (Employee employee : allEmployees) {
+            if (employee.getEmployeeID() == id) {
+                return employee;
+            }
+        }
+        return null;
+    }
+
+    @GetMapping("/employees?gender=male")
+    public ArrayList<Employee> getAllMaleEmployees(){
+        ArrayList<Employee> maleEmployees = new ArrayList<>();
+        for (Employee employee : allEmployees) {
+            if (Objects.equals(employee.getGender(), "male")) {
+                maleEmployees.add(employee);
+            }
+        }
+        return maleEmployees;
+    }
+
+
+
     @GetMapping("/employees")
-    public ArrayList<Employee> getAllEmployees(){
+    public ArrayList<Employee> getAllEmployees() {
         return allEmployees;
     }
 
 
-
-
     @PostMapping("/companies")
     @ResponseStatus(HttpStatus.CREATED)//返回创建成功的状态码
-    public void postCompany(@RequestBody Company company){
-        company.setId(companies.size()+1);
-        companies.put(companies.size()+1,company);
+    public void postCompany(@RequestBody Company company) {
+        company.setId(companies.size() + 1);
+        companies.put(companies.size() + 1, company);
     }
 
 }
